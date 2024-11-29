22f032d66253481bf146e2f78cee53b96ce06ba7
Yunus Emre Koçak <95947411+LightSenpai7@users.noreply.github.com>
2024-11-29 05:08:38 +0300

    Working Identity Register Backend

--- CashSoftware.PresentationLayer/Controllers/RegisterController.cs
+++ CashSoftware.PresentationLayer/Controllers/RegisterController.cs
@@ -35,8 +35,14 @@ namespace CashSoftware.PresentationLayer.Controllers
                 if (result.Succeeded) { 
                     return RedirectToAction("Index", "ConfirmMail");
                 }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
             }
-
             return View();
         }
     }
